7d5630daa66790e2fb3d9be294d0d60b7cbfc5b2
BavanthaU <bavanthau@eng.pdn.ac.lk>
2024-04-04 14:48:37 +0200

    changed rendering code and added a test checkpoint

--- Explore_PPO_agent_training.py
+++ Explore_PPO_agent_training.py
@@ -9,7 +9,7 @@ ray.init(ignore_reinit_error=True)
 
 
 
-chkpt_root = "tmp/ppo"
+chkpt_root = "tmp/ppo/checkpoint_{}"
 #shutil.rmtree(chkpt_root, ignore_errors=True, onerror=None)
 ray_results = "{}/ray_results/".format(os.getenv("HOME"))
 #shutil.rmtree(ray_results, ignore_errors=True, onerror=None)
@@ -21,11 +21,29 @@ config = PPOConfig().resources(num_gpus=0).rollouts(num_rollout_workers=24)
 
 agent = config.build(env=select_env)
 status = "{:2d} reward {:6.2f}/{:6.2f}/{:6.2f} len {:4.2f}"
+best_reward = float("-inf")
+best_checkpoint = None
+
 n_iter = 1000
+save_interval = 50
+warmpup_iterations = 50
+
 for n in range(n_iter):
     result = agent.train()
-    if n%50 ==0:
-        chkpt_file = agent.save(chkpt_root)
+    if n > warmpup_iterations:
+        if result["episode_reward_mean"] > best_reward:
+                best_reward = result["episode_reward_mean"]
+                if best_checkpoint:
+                    shutil.rmtree(chkpt_root.format("best"), ignore_errors=True, onerror=None)
+                best_checkpoint = agent.save(chkpt_root.format("best"))
+                print("Iteration {}: New Best Reward {:.2f}".format(
+                n + 1,
+                result["episode_reward_mean"],
+            ))
+                
+    if n%save_interval==0:
+        chkpt_file = agent.save(chkpt_root.format(n))
+
     print(status.format(
             n + 1,
             result["episode_reward_min"],
--- explore_agent_rollout.py
+++ explore_agent_rollout.py
@@ -4,7 +4,8 @@ import shutil
 import ray
 from ray.tune.registry import register_env
 from explore_agent.envs.exploring_gym import ExploreDrone
-import ray.rllib.agents.ppo as ppo
+import ray.rllib.algorithms.ppo.ppo as ppo
+from ray.rllib.algorithms.ppo.ppo import PPOConfig
 ray.init(ignore_reinit_error=True)
 from time import sleep
 
@@ -14,22 +15,23 @@ ray_results = "{}/ray_results/".format(os.getenv("HOME"))
 
 select_env = "ExploreAgent-v0"
 register_env(select_env, lambda config: ExploreDrone())
-config = ppo.DEFAULT_CONFIG.copy()
+config = PPOConfig().resources(num_gpus=0).rollouts(num_rollout_workers=24)
+
 
 config["log_level"] = "WARN"
-agent = ppo.PPOTrainer(config, env=select_env)
-chkpt_file = "tmp/ppo/checkpoint_001000"
+agent = ppo.PPO(config, env=select_env)
+chkpt_file = "tmp/ppo/checkpoint_best"
 
 agent.restore(chkpt_file)
 env = gym.make(select_env)
-state = env.reset()
+state,_ = env.reset()
 
 sum_reward = 0
 n_step = 500
 for step in range(n_step):
     action = agent.compute_single_action(state)
     sleep(0.0416)
-    state, reward, done, info = env.step(action)
+    state, reward, done, _,info = env.step(action)
     sum_reward += reward
     env.render()
     if done == 1:
